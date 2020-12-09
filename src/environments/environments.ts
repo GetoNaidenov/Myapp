7cfce32050b954e0b23e2c61038bd3d0027f7d3c
GetoNaidenov <55916204+GetoNaidenov@users.noreply.github.com>
2020-12-09 22:24:02 +0200

    Create environments.ts

---
+++ src/environments/environments.ts
@@ -0,0 +1,37 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.backendless.com/8420104C-DEE4-D18F-FF11-BB01B3AF8700/1BBC5146-E67C-4B6B-A7C8-907118F78B3C/data'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+// <script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyCRQNdFuW4YNHwlQIWLTnOB1eXCdB-sM3c",
+//     authDomain: "myapp-437d9.firebaseapp.com",
+//     projectId: "myapp-437d9",
+//     storageBucket: "myapp-437d9.appspot.com",
+//     messagingSenderId: "218881315421",
+//     appId: "1:218881315421:web:7c8ad9d395dd4a398d33b4"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+// </script>
