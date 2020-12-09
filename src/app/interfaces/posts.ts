6b1a7fe8f46884308badcb8e68de09b20ecf500d
GetoNaidenov <55916204+GetoNaidenov@users.noreply.github.com>
2020-12-09 22:29:55 +0200

    Add files via upload

---
+++ src/app/interfaces/posts.ts
@@ -0,0 +1,11 @@
+export interface IPost {
+    
+        post: string,
+        subscribe: number,
+        created: number,
+        name: string,
+        ___class: string,
+        ownerId: string,
+        updated: string,
+        objectId: string
+    }
